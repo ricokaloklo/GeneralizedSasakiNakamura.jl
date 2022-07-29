483753e18f6bdf8c59663db3bde76578bd639f5e
Rico Lo <ka-lok.lo@ligo.org>
2022-07-28 17:07:31 -0700

    Add kerr.jl that computes Kerr quantities

---
+++ src/kerr.jl
@@ -0,0 +1,16 @@
+function Delta(M, a, r)
+    r^2 - 2*M*r + a^2
+end
+
+function r_plus(M, a)
+    M + sqrt(M^2 - a^2)
+end
+
+function r_minus(M, a)
+    M - sqrt(M^2 - a^2)
+end
+
+function omega_horizon(M, a)
+    rp = r_plus(M, a)
+    return a/(2*M*rp)
+end
