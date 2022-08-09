624b0df0ff9040caa8dea07c383e03b1c67fabf0
Rico Lo <ka-lok.lo@ligo.org>
2022-08-08 17:26:25 -0700

    Fix typo; revert back to standard ODEProblem interface

--- src/solutions.jl
+++ src/solutions.jl
@@ -6,11 +6,19 @@ include("coordinates.jl")
 include("potentials.jl")
 include("initialconditions.jl")
 
-function generalized_sasaki_nakamura_equation(ddu, du, u, p, rs)
+function generalized_sasaki_nakamura_equation!(du, u, p, rs)
     r = r_from_rstar(p.a, rs)
     _sF = sF(p.s, p.m, p.a, p.omega, p.lambda, r)
     _sU = sU(p.s, p.m, p.a, p.omega, p.lambda, r)
-    @. ddu = _sF*du + _sU*u
+    #=
+    Using the convention for DifferentialEquations
+    u[1] = X(rs)
+    u[2] = dX/drs = X'
+    therefore
+    X'' - sF X' - sU X = 0 => u[2]' - sF u[2] - sU u[1] = 0 => u[2]' = sF u[2] + sU u[1]
+    =#
+    du[1] = u[2]
+    du[2] = _sF*u[2] + _sU*u[1]
 end
 
 function solve_Xinf(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-10, abstol=1e-10)
@@ -20,9 +28,10 @@ function solve_Xinf(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-10,
     end
     # Initial conditions at rs = rsout, the outer boundary
     Xinf_rsout, Xinfprime_rsout = Xinf_initialconditions(s, m, a, omega, lambda, rsout)
+    u0 = [Xinf_rsout; Xinfprime_rsout]
     rsspan = (rsout, rsin) # Integrate from rsout to rsin *inward*
-    p = (s=s, m=m, a=a, omega=a, lambda=lambda)
-    odeprob = SecondOrderODEProblem(generalized_sasaki_nakamura_equation, [Xinfprime_rsout], [Xinf_rsout], rsspan, p)
+    p = (s=s, m=m, a=a, omega=omega, lambda=lambda)
+    odeprob = ODEProblem(generalized_sasaki_nakamura_equation!, u0, rsspan, p)
     odealgo = RK4()
     odesoln = solve(odeprob, odealgo; reltol=reltol, abstol=abstol)
 end
@@ -36,7 +45,7 @@ function solve_XH(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-10, a
     XH_rsin, XHprime_rsin = XH_initialconditions(s, m, a, omega, lambda, rsin)
     u0 = [XH_rsin; XHprime_rsin]
     rsspan = (rsin, rsout) # Integrate from rsin to rsout *outward*
-    p = (s=s, m=m, a=a, omega=a, lambda=lambda)
+    p = (s=s, m=m, a=a, omega=omega, lambda=lambda)
     odeprob = ODEProblem(generalized_sasaki_nakamura_equation!, u0, rsspan, p)
     odealgo = RK4()
     odesoln = solve(odeprob, odealgo; reltol=reltol, abstol=abstol)
