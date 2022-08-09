91dcdf7824e47edcd8662e85abc66d53716f06f6
Rico Lo <ka-lok.lo@ligo.org>
2022-08-08 17:18:35 -0700

    Verified the transformation from X to R is correct

--- src/solutions.jl
+++ src/solutions.jl
@@ -6,42 +6,28 @@ include("coordinates.jl")
 include("potentials.jl")
 include("initialconditions.jl")
 
-function generalized_sasaki_nakamura_equation!(du, u, p, rs)
+function generalized_sasaki_nakamura_equation(ddu, du, u, p, rs)
     r = r_from_rstar(p.a, rs)
     _sF = sF(p.s, p.m, p.a, p.omega, p.lambda, r)
     _sU = sU(p.s, p.m, p.a, p.omega, p.lambda, r)
-    #=
-    Using the convention for DifferentialEquations
-    u[1] = X(rs)
-    u[2] = dX/drs = X'
-    therefore
-    X'' - sF X' - sU X = 0 => u[2]' - sF u[2] - sU u[1] = 0 => u[2]' = sF u[2] + sU u[1]
-    =#
-    du[1] = u[2]
-    du[2] = _sF*u[2] + _sU*u[1]
+    @. ddu = _sF*du + _sU*u
 end
 
-function solve_Xinf(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-8, abstol=1e-8)
+function solve_Xinf(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-10, abstol=1e-10)
     # Sanity check
     if rsin > rsout
         throw(DomainError(rsout, "rsout ($rsout) must be larger than rsin ($rsin)"))
     end
     # Initial conditions at rs = rsout, the outer boundary
     Xinf_rsout, Xinfprime_rsout = Xinf_initialconditions(s, m, a, omega, lambda, rsout)
-    u0 = [Xinf_rsout; Xinfprime_rsout]
     rsspan = (rsout, rsin) # Integrate from rsout to rsin *inward*
     p = (s=s, m=m, a=a, omega=a, lambda=lambda)
-    odeprob = ODEProblem(generalized_sasaki_nakamura_equation!, u0, rsspan, p)
-    #=
-    For now, we will stick to the 'simple' Runge-Kutta 4th order algorithm
-    In numerical recipes it was described that Bulirsch-Stoer algorithm is
-    'superior' to RK4 but it was very much disputed.
-    =#
+    odeprob = SecondOrderODEProblem(generalized_sasaki_nakamura_equation, [Xinfprime_rsout], [Xinf_rsout], rsspan, p)
     odealgo = RK4()
     odesoln = solve(odeprob, odealgo; reltol=reltol, abstol=abstol)
 end
 
-function solve_XH(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-8, abstol=1e-8)
+function solve_XH(s::Int, m::Int, a, omega, lambda, rsin, rsout; reltol=1e-10, abstol=1e-10)
     # Sanity check
     if rsin > rsout
         throw(DomainError(rsin, "rsin ($rsin) must be smaller than rsout ($rsout)"))
@@ -82,8 +68,8 @@ function Teukolsky_radial_function_from_Sasaki_Nakamura_function(s::Int, m::Int,
     end
     _chi_conv_array = chi_conversion_factor.(rgrid)
     _dchi_conv_dr_array = dchi_conversion_factor_dr.(rgrid)
-    chi = X .* _chi_conv_array
-    chiprime = Xprime .* _chi_conv_array + Xprime .* _dchi_conv_dr_array
+    @. chi = X * _chi_conv_array
+    @. chiprime = Xprime * _chi_conv_array + Xprime * _dchi_conv_dr_array
 
     # And then transform from chi, chiprime to R, Rprime
     _eta = (x -> eta(s, m, a, omega, lambda, x)).(rgrid)
@@ -94,10 +80,10 @@ function Teukolsky_radial_function_from_Sasaki_Nakamura_function(s::Int, m::Int,
     _Delta = (x -> Delta(a, x)).(rgrid)
     _VT = (x -> VT(s, m, a, omega, lambda, x)).(rgrid)
 
-    _inv_eta = 1.0 ./ _eta
+    @. _inv_eta = 1.0 / _eta
 
-    R = _inv_eta .* ( (_alpha .+ _beta_prime.*((_Delta).^(s+1))).*chi + (-_beta.*((_Delta).^(s+1))).*chiprime )
-    Rprime = _inv_eta .* ( -(_alpha_prime .+ _beta.*_VT.*((_Delta).^(s))).*chi + (_alpha).*chiprime )
+    @. R = _inv_eta * ( (_alpha + _beta_prime*((_Delta)^(s+1)))*chi + (-_beta*((_Delta)^(s+1)))*chiprime )
+    @. Rprime = _inv_eta * ( -(_alpha_prime + _beta*_VT*((_Delta)^(s)))*chi + (_alpha)*chiprime )
 
     return rgrid, rsgrid, R, Rprime
 end
