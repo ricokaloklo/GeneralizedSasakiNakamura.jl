dc317026b379273623bdaac86632be8be66b68ea
Rico Lo <ka-lok.lo@ligo.org>
2022-08-10 14:59:38 -0700

    Add LHS of radial Teukolsky equation

--- src/kerr.jl
+++ src/kerr.jl
@@ -18,3 +18,7 @@ end
 function K(m::Int, a, omega, r)
     (r^2 + a^2)*omega - m*a
 end
+
+function radial_Teukolsky_equation(s, m, a, omega, lambda, r, R, dRdr, d2Rdr2)
+    ((1 + s)*Delta(a ,r)^s*dRdr(r)*(2*(r-1)) + Delta(a, r)^(1 + s)*d2Rdr2(r))*(Delta(a,r)^(-s)) - VT(s, m, a, omega, lambda, r)*R(r)
+end
