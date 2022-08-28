b7cc21f1f2b9a3d770fa1566d7d7d83b8cd4eaef
Rico Lo <ka-lok.lo@ligo.org>
2022-08-27 17:17:34 -0700

    Package-ify

--- src/GeneralizedSasakiNakamura.jl
+++ src/GeneralizedSasakiNakamura.jl
@@ -1,5 +1,17 @@
 module GeneralizedSasakiNakamura
 
-# Write your package code here.
+include("solutions.jl")
+include("sourceterms.jl")
+include("regularization.jl")
+include("convolution.jl")
+
+# From solutions.jl
+export solve_Xinf, solve_Xhor, Teukolsky_radial_function_from_Sasaki_Nakamura_function
+# From sourceterms.jl
+export sourceterm_without_phasing_circularorbit, regularized_sourceterm_without_phasing_circularorbit
+# From regularization.jl
+export sourceterm_regularization_ansatz_coefficients, nearhorizon_ansatz
+# From convolution.jl
+export convolution_integral
 
 end
