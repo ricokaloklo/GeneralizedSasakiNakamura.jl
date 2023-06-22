45e37c0b1cdd5038b55b59eacb1edc102f0de803
Documenter.jl <documenter@juliadocs.github.io>
2023-06-22 07:13:25 +0000

    build based on c14f021

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Home","page":"Home","title":"Home","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"GeneralizedSasakiNakamura.jl computes solutions to the radial Teukolsky equation with Generalized Sasaki-Nakamura (GSN) formalism","category":"page"},{"location":"#License","page":"Home","title":"License","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"The package is licensed under the MIT License.","category":"page"}]
+}
